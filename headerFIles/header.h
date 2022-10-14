6c5d29b15cb9c3b27c67cdfac6b59dfdd79fde43
unknown <alaa.zingi.2004@gmail.com>
2022-10-14 22:26:38 +0300

    adding function prototypes

--- headerFIles/header.h
+++ headerFIles/header.h
@@ -1,14 +1,14 @@
 #ifndef Header
 #define Header
-int time();
 void init_board();
 void test_board();
 void printBoard();
 void choose();
 int fill_bin();
 void Color();
-int checkVertical() ;
+int checkVertical();
 int checkHorizental();
 int checkOblique();
 int check();
+int checkFull();
 #endif
