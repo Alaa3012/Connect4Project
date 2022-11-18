025249b60407cc5068c9dce320c785a7dd444bb1
maf <maf67@mail.aub.edu>
2022-11-18 23:35:15 +0200

    added the header file

---
+++ header.h
@@ -0,0 +1,14 @@
+#ifndef Header
+#define Header
+void init_board();
+void test_board();
+void printBoard();
+void choose();
+int fill_bin();
+void Color();
+int checkVertical();
+int checkHorizental();
+int checkOblique();
+int wonCheck();
+int checkIfFull();
+#endif
